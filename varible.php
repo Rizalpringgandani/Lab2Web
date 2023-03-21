efc53a4128416ba49bc9d5e84fd8c7e77cb74781
Rizalpringgandani <115872442+Rizalpringgandani@users.noreply.github.com>
2023-03-21 21:23:52 +0700

    Add files via upload

---
+++ varible.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+
+<body>
+    <h1>Variable</h1>
+    <?php
+    $nim = "312110151";
+    $nama = 'Rizal Pringgandani';
+    echo "NIM : " . $nim . "<br>";
+    echo "Nama : $nama";
+
+    ?>
+</body>
+
+</html>
